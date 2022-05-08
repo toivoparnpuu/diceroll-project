2b7985a7fa59e8547db846c6546dcd990bfa75fb
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-05-08 13:13:46 +0300

    Code formating

--- script.js
+++ script.js
@@ -12,40 +12,38 @@ const writeout = document.querySelector(selectors.writeout);
 
 
 //event listeners
-resetButton.addEventListener("click", function() {
+resetButton.addEventListener("click", function () {
     resetInputAndLog();
-  });
-  
-rollButton.addEventListener("click", function(){
+});
+
+rollButton.addEventListener("click", function () {
     let rollingList = parseDices(input.value);
     rollDice(rollingList);
     rollingList.forEach(element => {
-        displayResults(element);    
+        displayResults(element);
     });
-    
+
 });
 
-
-
 // functions
-function resetInputAndLog(){
+function resetInputAndLog() {
     input.value = "";
-    //https://www.javascripttutorial.net/javascript-dom/javascript-removechild/
-    while (writeout.firstChild){
+    while (writeout.firstChild) {
         writeout.removeChild(writeout.firstChild);
     }
 }
-function parseDices(dicesInput){
+
+function parseDices(dicesInput) {
     let result = dicesInput.trim().split('+');
     return result;
 }
 
-function rollDice(){
-    
+function rollDice() {
+
     console.log("TODO");
 }
 
-function displayResults(textToOutput){
+function displayResults(textToOutput) {
     let li = document.createElement('li');
     li.appendChild(document.createTextNode(textToOutput));
     writeout.appendChild(li);
@@ -53,5 +51,4 @@ function displayResults(textToOutput){
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
-  }
-  
+}
