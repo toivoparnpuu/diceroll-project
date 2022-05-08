95309d77b2f7ba3da161331e2e07843cfe315b61
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-05-08 14:16:12 +0300

    Rolling dice implementation

--- script.js
+++ script.js
@@ -10,22 +10,36 @@ const rollButton = document.querySelector(selectors.rollButton);
 const input = document.querySelector(selectors.input);
 const writeout = document.querySelector(selectors.writeout);
 
-
 //event listeners
 resetButton.addEventListener("click", function () {
     resetInputAndLog();
 });
 
 rollButton.addEventListener("click", function () {
-    let rollingList = parseDices(input.value);
-    rollDice(rollingList);
-    rollingList.forEach(element => {
-        displayResults(element);
-    });
-
+    clickOnRollDice();
 });
 
 // functions
+function clickOnRollDice() {
+    let rollingList = parseDices(input.value);
+    let diceTotal = 0;
+    diceList = [];
+    
+    rollingList.forEach(element => {
+        let diceCode = element.split('d')[1];
+        let diceRolls = element.split('d')[0];
+
+        for (let i = 0; i < diceRolls; i++) {
+            let diceResult = getRandomInt(diceCode);
+            diceList.push(diceResult[0]);
+            displayResults('d' + diceCode + ': ' + diceResult.join(' '));
+            diceTotal = diceTotal + diceResult[0];
+        }
+    });
+
+    displayResults('KOKKU: ' + diceList.join(' + ') + ' = ' + diceTotal);
+}
+
 function resetInputAndLog() {
     input.value = "";
     while (writeout.firstChild) {
@@ -38,17 +52,24 @@ function parseDices(dicesInput) {
     return result;
 }
 
-function rollDice() {
-
-    console.log("TODO");
-}
-
 function displayResults(textToOutput) {
     let li = document.createElement('li');
     li.appendChild(document.createTextNode(textToOutput));
     writeout.appendChild(li);
-}
+} 
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max) + 1;
+    let extraMsg = '';
+    let diceRoll = Math.floor(Math.random() * max) + 1;
+
+    if (max === 20) {
+        if (diceRoll === 1){
+            extraMsg = '(critical miss)';
+        }
+        else if (diceRoll === 20) {
+            extraMsg = '(critical hit)';
+        }
+    };
+
+    return [diceRoll, extraMsg];
 }
